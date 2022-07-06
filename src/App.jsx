111cb2549d8baad6c80473bac4aab288a7b06b59
Dan Ranges <danranges@gmail.com>
2022-07-06 10:09:45 +0200

    Add MUI icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { Typography } from '@mui/material';
-import { useState } from 'react';
+import { AddShoppingCart } from '@mui/icons-material';
+import { Button, IconButton, Typography } from '@mui/material';
 
 const App = () => {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Typography variant='h1'>test!</Typography>
+        <Typography variant='h1'>Hello!</Typography>
+        <IconButton>
+          <AddShoppingCart />
+        </IconButton>
       </header>
     </div>
   );
